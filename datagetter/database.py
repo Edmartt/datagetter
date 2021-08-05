920210b67e5a047dddf7e7bb1b719180ce766a03
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-05 16:31:01 -0500

    Now the method is inside a class and also added method return type

--- datagetter/database.py
+++ datagetter/database.py
@@ -4,7 +4,8 @@ import mysql.connector
 
 class Database():
 
-    def create_connection(self):
+    @staticmethod
+    def create_connection() -> mysql.connector.MySQLConnection:
         connection = mysql.connector.connect(
             host=environ.get('MYSQL_HOST'),
             user=environ.get('MYSQL_USER'),
@@ -15,25 +16,3 @@ class Database():
         cursor = connection.cursor(prepared=True)
 
         return connection, cursor
-
-#    def create_table(self):
-#        querys = [
-#                'DROP TABLE IF EXISTS playersdata;',
-#                """CREATE TABLE IF NOT EXISTS playersdata (id INT UNSIGNED NOT NULL
-#                PRIMARY KEY
-#                AUTO_INCREMENT, name VARCHAR(60) NOT NULL, position VARCHAR(60)
-#                NOT NULL, nation VARCHAR(60) NOT NULL, club VARCHAR(60) NOT NULL)
-#                DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"""
-#                ]
-#
-#        connection, cursor = self.create_connection()
-#
-#        try:
-#            for query in querys:
-#                cursor.execute(query)
-#            connection.commit()
-#            print('Tables created')
-#        except mysql.connector.Error as ex:
-#            print(ex)
-#        finally:
-#            connection.close()
