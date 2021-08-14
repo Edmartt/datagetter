eff9fad4eced271f83afa3014a70f3760aaf0316
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-13 23:20:07 -0500

    Applied code refactoring and added method docstring

--- datagetter/database.py
+++ datagetter/database.py
@@ -3,15 +3,25 @@ import mysql.connector
 
 
 class Database():
+    __host = environ.get('MYSQL_HOST')
+    __user = environ.get('MYSQL_USER')
+    __password = environ.get('MYSQL_PASSWORD')
+    __database = environ.get('MYSQL_DATABASE')
+    __port = environ.get('MYSQL_PORT')
 
     @staticmethod
     def create_connection() -> mysql.connector.MySQLConnection:
+        '''Creates database connection.
+
+        Takes the class variables as parameters to return a
+        successful database connection.
+        '''
         connection = mysql.connector.connect(
-            host=environ.get('MYSQL_HOST'),
-            user=environ.get('MYSQL_USER'),
-            password=environ.get('MYSQL_PASSWORD'),
-            database=environ.get('MYSQL_DATABASE'),
-            port=environ.get('MYSQL_PORT')
+            host=Database.__host,
+            user=Database.__user,
+            password=Database.__password,
+            database=Database.__database,
+            port=Database.__port
             )
         cursor = connection.cursor(prepared=True)
 
