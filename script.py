dc908f6617783f5fca659ffcc8f06c1f706cbbb9
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-05-13 22:24:57 -0500

    refactor: updates to class approach

--- script.py
+++ script.py
@@ -1,46 +1,60 @@
 """Scrpit para extraer los datos de los jugadores de la API FUT21."""
 
+import os
 import requests
 from datagetter.players_data import Players
 
 
-def get_data(url: str =
-             "https://www.easports.com/fifa/ultimate-team/api/fut/item",
-             page: int = 1) -> None:
+class DataGetter:
 
-    players_data = Players()
+    def __init__(self, query_gen: Players, base_url: str) -> None:
+        self.query_gen = query_gen
+        self.page = 1
+        self.base_url = base_url
 
-    args = {'page': page} if page else {}
+    def get_players_data(self) -> dict:
 
-# listas con las propiedades de los jugadores
-    player_names = []
-    players_positions = []
-    players_nations = []
-    players_clubs = []
+        args = {'pageCurrent': self.page} if self.page else {}
+        headers = {'X-AUTH-TOKEN':os.environ.get('X_AUTH_TOKEN')}
+        response = requests.get(url=self.base_url+'/players', params=args, headers=headers).json()
 
-# Obtenemos la respuesta de la API y la serializamos a json
-    response = requests.get(url, params=args).json()
+        return response
 
-# Iteramos sobre la estructura items del response para sacar
-# los datos solicitados en el ejercicio
-    for i in response['items']:
 
-        player_names.append(i['firstName'] + ' ' + i['lastName'])
-        players_positions.append(i['position'])
-        players_nations.append(i['nation']['name'])
-        players_clubs.append(i['club']['name'])
+    def save_data_to_db(self, data_object: dict) -> None:
 
-    players_data.insert_data(player_names, players_positions,
-                             players_nations, players_clubs)
+        player_names = []
+        players_positions = []
+        players_nations = []
+        players_clubs = []
+
+        for i in data_object['items']:
+            player_names.append(i['name'])
+            players_positions.append(i['position'])
+            players_nations.append(i['nation'])
+            players_clubs.append(i['club'])
+        self.query_gen.insert_data(player_names, players_positions, players_nations, players_clubs)
+    
+    def go_to_next_page(self):
+        while True:
+            choice = input('Do you want to read the next page (Y/N): '.lower())
+
+            if choice == 'y' and data_getter.page <= 908: 
+                data_getter.page += 1
+                data = data_getter.get_players_data()
+                data_getter.save_data_to_db(data_object=data)
+            else:
+                break
+
 
 # Una vez extraidos los datos de la primera pagina, se nos pregunta
 # si queremos continuar leyendo los datos de la siguiente pagina y guardarlos
 
-    choice = input('Do you want to read the next page (Y/N): '.lower())
-    page += 1
-    if choice == 'y' and page <= 908:
-        get_data(url, page)
-        return
 
+data_getter = DataGetter(Players(), os.environ.get('BASE_URL'))
 
-get_data()
+data = data_getter.get_players_data()
+
+data_getter.save_data_to_db(data_object=data)
+
+data_getter.go_to_next_page()
